a73837678c6863899a9eb99f8f0fb6265ccb265e
khadijanadeem442 <khadijanadeem442@gmail.com>
2025-09-09 08:43:18 +0500

    Delete home.js
    parent ki bannai

--- home.js
+++
@@ -1,9 +0,0 @@
-const slider = document.querySelector('.special-box');
-const images = slider.querySelectorAll('img');
-let currentImage = 0;
-
-function showNextImage() {
-    images[currentImage].classList.remove('active');
-    currentImage = (currentImage + 1) % images.length;
-    images[currentImage].classList.add('active');
-}
