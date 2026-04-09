3eaf72269936b3895d66cc7e2c18f7b8e355bace
attiatulbari <romeattia@gmail.com>
2026-04-09 22:13:02 +0500

    Update dashboard_manager.php

--- dashboard_manager.php
+++ dashboard_manager.php
@@ -3,7 +3,7 @@ session_start();
 
 // Database connection
 $conn = new mysqli("localhost","root","","hak_dine");
-if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+if ($conn->connect_error) die("Connection failed: "  . $conn->connect_error);
 
 
 // Function to safely get single stat
